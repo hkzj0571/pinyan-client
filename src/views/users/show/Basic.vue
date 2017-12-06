0bc2984fe444dc498e294590d2e006e2511453e1
Seaony <seaony@outlook.com>
2017-12-06 19:07:31 +0800

    :tada: Update the api resource

--- src/views/users/show/Basic.vue
+++ src/views/users/show/Basic.vue
@@ -4,12 +4,12 @@
         <div class="container">
             <i-col :span="16" class="left">
                 <div class="user-detail">
-                    <router-link class="avatar" :to="'/user/'+id+'/profile'">
+                    <router-link class="avatar" :to="{name:'user.show.profile',params:{user:id}}">
                         <img :src="user.avatar">
                     </router-link>
                     <div class="info">
                         <span class="name">
-                            <router-link :to="'/user/'+id+'/profile'" v-text="user.name"></router-link>
+                            <router-link :to="{name:'user.show.profile',params:{user:id}}" v-text="user.name"></router-link>
                         </span>
                         <Icon :type="user.gender" v-show="user.gender"></Icon>
                         <ul class="options">
@@ -31,10 +31,10 @@
                             </li>
                             <li>
                                 <div class="meta-block">
-                                    <a href="/users/06201a8515a3/following">
+                                    <router-link :to="{name:'user.show.profile',params:{user:id}}">
                                         <p>10</p>
                                         文章
-                                    </a>
+                                    </router-link>
                                 </div>
                             </li>
                         </ul>
@@ -47,21 +47,23 @@
                     <span>个人介绍</span>
                 </div>
                 <div class="description">
-                    <div class="resume" v-text="user.resume"></div>
+                    <div class="resume" v-html="user.resume"></div>
                 </div>
                 <ul class="synamic">
                     <li>
                         <a href="/users/06201a8515a3/subscriptions">
                             <Icon type="android-person"></Icon>
-                            关注的用户</a>
+                            关注的用户
+                        </a>
                     </li>
                     <li>
-                        <a href="/users/06201a8515a3/subscriptions">
+                        <router-link :to="{name:'user.show.focus',params:{user:id}}">
                             <Icon type="android-bookmark"></Icon>
-                            关注的专题</a>
+                            关注的专题
+                        </router-link>
                     </li>
                     <li>
-                        <router-link :to="'/user/'+id+'/focus'">
+                        <router-link :to="{name:'user.show.focus',params:{user:id}}">
                             <Icon type="android-favorite-outline"></Icon>
                             喜欢的文章
                         </router-link>
@@ -70,13 +72,13 @@
                 <div class="topics_options">
                     <div class="title">
                         我创建的专题
-                        <router-link to="/topics">
+                        <router-link :to="{name:'topics.create'}">
                             <Button type="text" icon="plus-round">创建一个新专题</Button>
                         </router-link>
                     </div>
                     <ul class="collection">
                         <li v-for="topic in user.create_topics">
-                            <router-link :to="'/topics/'+topic.id">
+                            <router-link :to="{name:'topics.index',params:{topic:topic.id}}">
                                 <Avatar icon="person" shape="square" :src="topic.cover"></Avatar>
                                 {{ topic.name }}
                             </router-link>
